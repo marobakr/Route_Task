641f56f5c52dfabe8fb7b23a3d5c1b3c2d3eccbd
marwanabubakr <dev@marobakr>
2024-07-14 00:57:28 +0300

    last test Server

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { isDevMode } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +8,7 @@ import { isDevMode } from '@angular/core';
 export class DataService {
   constructor(private _httpClient: HttpClient) {}
 
-  private baseApi = isDevMode()
-    ? 'http://localhost:3000'
-    : 'https://route-task-mauve.vercel.app/api/json-server';
+  private baseApi = 'https://route-task-mauve.vercel.app';
 
   inputValueSubject = new BehaviorSubject<number>(0);
   resultOfSearch = new BehaviorSubject<number>(0);
